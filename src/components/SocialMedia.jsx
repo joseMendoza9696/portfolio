8a9d035b1c7d334d39dc64ba3c4324a13d9e71f6
Jose Mendoza <joseMendoza9696>
2022-12-03 15:07:55 -0400

    fixing deployment bug

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { BsTwitter, BsInstagram, BsLinkedin } from 'react-icons/bs';
-import { FaFacebookF } from 'react-icons/fa';
+import { BsLinkedin } from 'react-icons/bs';
 
 const SocialMedia = () => (
 	<div className="app__social">
